c5d5082ea5e4cca9c3e6a2c38d644be88fec4b76
Blaine Nguyen <m.khang8123@gmail.com>
2025-04-23 07:28:49 +0700

    khang

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  BASE_URL: 'http://192.168.68.104:3000', // Replace with your production URL later
-  SOCKET_SERVER_URL: 'http://192.168.68.104:3000'
+  BASE_URL: 'http://192.168.90.161:3000', // Replace with your production URL later
+  SOCKET_SERVER_URL: 'http://192.168.90.161:3000'
 }
 
 export default config
